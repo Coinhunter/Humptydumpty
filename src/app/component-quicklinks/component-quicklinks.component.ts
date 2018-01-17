8c67346722138bfc8bf28fe766d3dce6d532610c
Coinhunter <conrad.ljungstrom@gmail.com>
2018-01-17 18:36:21 +0100

    snabblänkar lite granna

--- src/app/component-quicklinks/component-quicklinks.component.ts
+++ src/app/component-quicklinks/component-quicklinks.component.ts
@@ -7,6 +7,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ComponentQuicklinksComponent implements OnInit {
 
+  quicklinks = [
+    {
+      headline: 'Populära jobbplaceringar',
+      items: [
+        {
+          name: 'Göteborg',
+          tags: [1235],
+        },
+        {
+          name: 'Stockholm',
+          tags: [1234],
+        },
+        {
+          name: 'Malmö',
+          tags: [1233],
+        },
+        {
+          name: 'Hela Sverige',
+          tags: [],
+        },
+      ]
+    },
+    {
+      headline: 'Populära yrken',
+      items: [
+        {
+          name: 'Hotell & Restaurang',
+          tags: [],
+        },
+        {
+          name: 'Administration',
+          tags: [],
+        },
+        {
+          name: 'Hälsa och Sjukvård',
+          tags: [],
+        },
+        {
+          name: 'Data och IT',
+          tags: [],
+        },                        
+      ]
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
