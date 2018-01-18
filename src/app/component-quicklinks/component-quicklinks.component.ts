35b6fa8008e8cb15ff9cfcb2b27e2f300a909804
Coinhunter <conrad.ljungstrom@gmail.com>
2018-01-18 13:36:25 +0100

    Restaurangbranchen snabblänk

--- src/app/component-quicklinks/component-quicklinks.component.ts
+++ src/app/component-quicklinks/component-quicklinks.component.ts
@@ -40,19 +40,60 @@ export class ComponentQuicklinksComponent implements OnInit {
       items: [
         {
           name: 'Hotell & Restaurang',
-          tags: [],
+          url: this.getUrlForCriteria([
+            new Profilkriterium('5132', 'Bartendrar', 'YRKESGRUPP_ROLL'),
+            new Profilkriterium('5131', 'Hovmästare och servitörer', 'YRKESGRUPP_ROLL'),
+            new Profilkriterium('9413', 'Kafé- och konditoribiträden', 'YRKESGRUPP_ROLL'),
+            new Profilkriterium('5120', 'Kockar och kallskänkor', 'YRKESGRUPP_ROLL'),
+            new Profilkriterium('3451', 'Köksmästare och souschefer', 'YRKESGRUPP_ROLL'),
+            new Profilkriterium('9411', 'Pizzabagare m.fl.', 'YRKESGRUPP_ROLL'),
+            new Profilkriterium('9412', 'Restaurang- och köksbiträden m.fl.', 'YRKESGRUPP_ROLL'),
+            new Profilkriterium('1720', 'Restaurang- och kökschefer', 'YRKESGRUPP_ROLL'),            
+          ]),
         },
         {
           name: 'Administration',
-          tags: [],
+          url: '',
         },
         {
           name: 'Hälsa och Sjukvård',
-          tags: [],
+          url: this.getUrlForCriteria([
+            new Profilkriterium('5326', 'Ambulanssjukvårdare', 'yrkesgrupper'),
+            new Profilkriterium('5325', 'Barnsköterskor', 'yrkesgrupper'),
+            new Profilkriterium('5341', 'Skötare', 'yrkesgrupper'),
+            new Profilkriterium('5321', 'Undersköterskor, hemtjänst, äldreboende m.fl.', 'yrkesgrupper'),
+            new Profilkriterium('5323', 'Undersköterskor, vård- o specialavd o mottagning', 'yrkesgrupper'),
+            new Profilkriterium('5330', 'Vårdbiträden', 'yrkesgrupper'),
+            new Profilkriterium('5349', 'Övrig vård- och omsorgspersonal', 'yrkesgrupper'),
+            new Profilkriterium('5311', 'Barnskötare', 'yrkesgrupper'),
+            new Profilkriterium('2273', 'Arbetsterapeuter', 'yrkesgrupper'),
+            new Profilkriterium('2213', 'AT-läkare', 'yrkesgrupper'),
+            new Profilkriterium('2222', 'Barnmorskor', 'yrkesgrupper'),
+            new Profilkriterium('2232', 'Barnsjuksköterskor', 'yrkesgrupper'),
+            new Profilkriterium('2224', 'Distriktssköterskor', 'yrkesgrupper'),
+            new Profilkriterium('2272', 'Fysioterapeuter och sjukgymnaster', 'yrkesgrupper'),
+            new Profilkriterium('2221', 'Grundutbildade sjuksköterskor', 'yrkesgrupper'),
+            new Profilkriterium('2228', 'Intensivvårdssjuksköterskor', 'yrkesgrupper'),
+            new Profilkriterium('2231', 'Operationssjuksköterskor', 'yrkesgrupper'),
+            new Profilkriterium('2225', 'Psykiatrisjuksköterskor', 'yrkesgrupper'),
+            new Profilkriterium('2241', 'Psykologer', 'yrkesgrupper'),
+            new Profilkriterium('2242', 'Psykoterapeuter', 'yrkesgrupper'),
+            new Profilkriterium('2235', 'Röntgensjuksköterskor', 'yrkesgrupper'),
+            new Profilkriterium('2233', 'Skolsköterskor', 'yrkesgrupper'),
+            new Profilkriterium('2211', 'Specialistläkare', 'yrkesgrupper'),
+            new Profilkriterium('2212', 'ST-läkare', 'yrkesgrupper'),
+            new Profilkriterium('2239', 'Övriga specialistsjuksköterskor', 'yrkesgrupper'),
+            new Profilkriterium('2219', 'Övriga läkare', 'yrkesgrupper'),
+            new Profilkriterium('2289', 'Övriga specialister inom hälso- och sjukvård', 'yrkesgrupper'),
+            new Profilkriterium('3212', 'Biomedicinska analytiker m.fl.', 'yrkesgrupper'),
+            new Profilkriterium('2223', 'Anestesisjuksköterskor', 'yrkesgrupper'),
+            new Profilkriterium('2226', 'Ambulanssjuksköterskor m.fl.', 'yrkesgrupper'),
+            new Profilkriterium('2227', 'Geriatriksjuksköterskor', 'yrkesgrupper'),
+          ]),
         },
         {
           name: 'Data och IT',
-          tags: [],
+          url: '',
         },                        
       ]
     }
@@ -99,7 +140,7 @@ export class ComponentQuicklinksComponent implements OnInit {
     url = url.slice(1, url.length - 1);
     url = '?' + url;
 
-    return url;
+    return this.baseUrl + url;
   }
 
   ngOnInit() {
