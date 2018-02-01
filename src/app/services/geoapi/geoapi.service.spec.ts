f4107f2fad8db0f445bcf2193f8115a7218b563b
Coinhunter <conrad.ljungstrom@gmail.com>
2018-02-01 10:01:34 +0100

    search service fix tests

--- src/app/services/geoapi/geoapi.service.spec.ts
+++ src/app/services/geoapi/geoapi.service.spec.ts
@@ -4,6 +4,7 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { GeoapiService } from './geoapi.service';
 import { Profilkriterium } from '../../models/Profilkriterium';
+import { HttpErrorResponse } from '@angular/common/http/src/response';
 
 describe('GeoapiService', () => {
   beforeEach(() => {
@@ -23,17 +24,22 @@ describe('GeoapiService', () => {
 
   // Any city and number would do, just making sure the API answers proprtly..
   it('should contain more than 30 entries for "Skara" - 532.', async(inject([HttpClientModule, GeoapiService], (http: HttpClientModule, service: GeoapiService) => {
-    service.getOrtByPostnummer('532').then((result) => {
+    service.getOrtByPostnummer('532').subscribe((result) => {
+      console.log(result);
       expect(result).toBeDefined();
       expect(result.length).toBeGreaterThan(30);
     });
   })));
 
   it('should throw an error when supplying too short filter', async(inject([HttpClientModule, GeoapiService], (http: HttpClientModule, service: GeoapiService) => {
-    service.getOrtByPostnummer('53').then(result => {
+    service.getOrtByPostnummer('53').subscribe((data) => {
       fail('Unwanted code block');
-    }).catch((error) => {
-      expect(error).toEqual(new Error('Tre till fem siffror måste anges.'));
+    }, (err:HttpErrorResponse) => {
+      console.log(err);
+      
+      err.statusText
+
+      expect(err.message).toEqual('Tre till fem siffror måste anges.');
     });
   })));
 
